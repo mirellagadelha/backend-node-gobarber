ad833c035f5dfb73b323a3f7a73a3c69df2f6e17
Mirella Gadelha Santos <mirella.gadelha@svm.com.br>
2020-07-19 14:43:44 -0300

    Validating appointment data to not allow appointment scheduling at the same time.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,35 @@
 import Router from "express";
 import { uuid } from "uuidv4";
+import { startOfHour, parseISO, isEqual } from "date-fns";
+
+interface Appointment {
+  id: String;
+  provider: String;
+  date: Date;
+}
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: "This appointment is already booked." });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
