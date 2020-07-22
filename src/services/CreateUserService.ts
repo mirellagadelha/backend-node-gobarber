abc9ac4438fd98157b91384f72904735ee70d2e6
Mirella Gadelha <mirella.gadelha.s@gmail.com>
2020-07-22 14:25:47 -0300

    Creating route and service to create user.

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from 'typeorm';
+import { response } from 'express';
+import User from '../models/User';
+
+interface RequestDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: RequestDTO): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUserExists = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw Error('Email address already used.');
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
