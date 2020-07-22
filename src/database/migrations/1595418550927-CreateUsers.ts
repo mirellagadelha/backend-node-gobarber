eeeba936d0737373e2fbf430702c951dac66e4ad
Mirella Gadelha <mirella.gadelha.s@gmail.com>
2020-07-22 12:14:40 -0300

    Establishing relationships between the Appointments and Users classes.

--- src/database/migrations/1595418550927-CreateUsers.ts
+++ src/database/migrations/1595418550927-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1595352272766
-  implements MigrationInterface {
+export default class CreateUsers1595418550927 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,13 +14,18 @@ export default class CreateAppointments1595352272766
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
           },
           {
@@ -40,6 +44,6 @@ export default class CreateAppointments1595352272766
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
