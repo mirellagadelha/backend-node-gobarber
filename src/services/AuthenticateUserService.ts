4ee8cd67867e2a9eb234b57c68a10fd34622cfa1
Mirella Gadelha <mirella.gadelha.s@gmail.com>
2020-07-24 12:55:42 -0300

    Created class AppError to handle errors on routes.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -5,6 +5,8 @@ import authConfig from '../config/auth';
 
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 interface RequestDTO {
   email: string;
   password: string;
@@ -22,13 +24,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
